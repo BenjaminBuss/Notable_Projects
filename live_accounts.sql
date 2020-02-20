4262581fa95fd91d7de459c435135000110c7eba
BenjaminBuss <25209752+BenjaminBuss@users.noreply.github.com>
2020-02-20 15:01:53 -0700

    Delete live_accounts.sql

--- live_accounts.sql
+++
@@ -1,5 +0,0 @@
-SELECT COUNT(DISTINCT pro_id) AS live_accounts, SUM(total) AS billables, year, month 
-FROM billing_invoices 
-WHERE year > 2016 AND brand_id <> 27
-GROUP BY month, year 
-ORDER BY year ASC, month ASC
