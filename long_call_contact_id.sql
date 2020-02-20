7f14f4f39890b5177d2fc22e042e9400c7772412
BenjaminBuss <25209752+BenjaminBuss@users.noreply.github.com>
2020-02-20 15:03:31 -0700

    Delete long_call_contact_id.sql

--- long_call_contact_id.sql
+++
@@ -1,8 +0,0 @@
-SELECT COUNT(*) AS count, contact_id
-FROM (SELECT time_to_sec(timediff(released, datestamp)) AS call_length, callcenter, disposition, contact_id
-	FROM call_log
-	WHERE origin = 'in' AND callcenter = 'ShelfGenieCC' AND disposition <> 1
-	HAVING(call_length > 120)) AS sub
-GROUP BY contact_id
-ORDER BY count DESC;
-
